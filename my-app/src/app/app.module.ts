fd616c3eb250f7ade7305d61dcd4b4150f381122
jtrojahn <Justin.M.Trojahn@studmail.w-hs.de>
2022-05-12 16:30:06 +0200

    Setup 2

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { TemperatureBoxComponent } from './temperature-box/temperature-box.component';
+import { HumidityBoxComponent } from './humidity-box/humidity-box.component';
+import { TemperatureGraphComponent } from './temperature-graph/temperature-graph.component';
+import { HumidityGraphComponent } from './humidity-graph/humidity-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    TemperatureBoxComponent,
+    HumidityBoxComponent,
+    TemperatureGraphComponent,
+    HumidityGraphComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/humidity-graph/humidity-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-humidity-graph',
+  templateUrl: './humidity-graph.component.html',
+  styleUrls: ['./humidity-graph.component.css']
+})
+export class HumidityGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
